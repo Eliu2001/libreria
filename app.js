77d1db3d32704a96639178e6d4ab39395b1313e1
Eliu2001 <villalobosgermain2@gmail.com>
2025-11-23 15:22:47 -0300

    feat: add views for home, books, login, and registration with Bootstrap styling

--- app.js
+++ app.js
@@ -26,5 +26,5 @@ await connectDB();
 await sequelize.sync();
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Servidor arriba en puerto 3000 ✔");
+    console.log("Servidor arriba en puerto 3000");
 });
