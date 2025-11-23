508fb1660050687de99a9058c2e842d1a10a5a30
Eliu2001 <villalobosgermain2@gmail.com>
2025-11-23 13:26:11 -0300

    feat: implement authentication controller with login, register, and logout functionality

---
+++ controllers/authController.js
@@ -0,0 +1,49 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { User } from '../models/User.js';
+
+export const showLogin = (req, res) => {
+    res.render('login');
+};
+
+export const showRegister = (req, res) => {
+    res.render('register');
+};
+
+export const register = async (req, res) => {
+    const { username, password } = req.body;
+
+    const hash = bcrypt.hashSync(password, 10);
+
+    await User.create({ username, password: hash});
+
+    res.redirect('/login');
+};
+
+export const login = async (req, res) => {
+    const { username, password} = req.body;
+
+    const user = await User.findOne({ where: { username }});
+
+    if(!user) {
+        return res.redirect('login', { error: 'User does not exist' });
+    }
+    const verifyPassword = bcrypt.compareSync(password, user.password);
+
+    if(!verifyPassword) {
+        return res.redirect('login', { error: 'Invalid credentials' });
+    }
+
+    const token = jwt.sign(
+        { id: user.id, username: user.username },
+        process.env.JWT_SECRET,
+        { expiresIn: '1h' }
+    );
+    res.cookie('token', token);
+    res.redirect('/');
+}
+
+export const logout = (req, res) => {
+    res.clearCookie('token');
+    res.redirect('/login');
+};
