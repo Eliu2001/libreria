3ce5ad8647e800f8308595f34e9538311a4a5feb
Eliu2001 <villalobosgermain2@gmail.com>
2025-11-23 13:45:32 -0300

    feat: initialize Express app and set up authentication routes

---
+++ app.js
@@ -0,0 +1,30 @@
+import e from 'express';
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser';
+import exphbs from 'express-handlebars';
+
+import { connectDB } from './config/database';
+import authRoutes from './routes/authRoutes.js';
+import booksRoutes from './routes/booksRoutes.js';
+
+dotenv.config();
+
+const app = e();
+app.use(e.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(e.static("public"));
+
+app.engine("hbs", exphbs.engine({ extname: ".hbs" }));
+app.set("view engine", "hbs");
+
+app.use(authRoutes);
+app.use(booksRoutes);
+
+app.get("/", (req, res) => res.render("home"));
+
+await connectDB();
+await sequelize.sync();
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Servidor arriba en puerto 3000 ✔");
+});
---
+++ routes/authRoutes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { showLogin, showRegister, register, login, logout } from "../controllers/authController";
+
+const router = Router();
+
+router.get("/login", showLogin);
+router.post("/login", login);
+
+router.get("/register", showRegister);
+router.post("/register", register);
+router.get("/logout", logout);
+
+export default router;
